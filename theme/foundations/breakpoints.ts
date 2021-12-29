567b5fb912fa18794388f84e01502b0e925de9ad
Jake Giri <neelunstp@gmail.com>
2021-12-30 03:34:27 +0530

    to add: breakpoints: commented in theme

--- theme/foundations/breakpoints.ts
+++ theme/foundations/breakpoints.ts
@@ -2,6 +2,7 @@ import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 export const breakpoints = createBreakpoints({
   sm: "480px",
+  //375 //414
   md: "768px",
   lg: "992px",
   xl: "1280px",
